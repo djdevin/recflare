8e644a5c200583267c571b46463f74af621359af
Devin Zuczek <devin.z@purple.com>
2026-06-08 18:23:40 -0400

    Initial commit

---
+++ turbo/generators/plugins/fix-all.ts
@@ -0,0 +1,21 @@
+import { $ } from 'zx'
+
+import { catchError, onProcSuccess } from '../helpers/proc'
+import { slugifyText } from '../helpers/slugify'
+
+import type { PlopTypes } from '@turbo/gen'
+import type { Answers } from '../answers'
+
+export function fixAll(answers: Answers, _config: any, _plop: PlopTypes.NodePlopAPI) {
+	return new Promise((resolve, reject) => {
+		console.log('🌀 running pnpm fix...')
+
+		$({
+			cwd: answers.turbo.paths.root,
+			nothrow: true,
+			quiet: true,
+		})`pnpm -F ${slugifyText(answers.name)} check:lint --fix && pnpm runx fix --deps --format --workers-types`
+			.then(onProcSuccess('pnpm fix', resolve, reject))
+			.catch(catchError(reject))
+	})
+}
